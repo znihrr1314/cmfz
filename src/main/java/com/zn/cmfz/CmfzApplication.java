1c35067cda823442a3efaf755ecb799699cdc75d
znihrr1314 <zn956889576@163.com>
2018-11-27 13:55:56 +0800

    第一次提交

---
+++ src/main/java/com/zn/cmfz/CmfzApplication.java
@@ -0,0 +1,12 @@
+package com.zn.cmfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzApplication.class, args);
+    }
+}
